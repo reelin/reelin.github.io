c9fd395486ecd7635b32f95d0de91e32a494d134
reelin <reelin.rl@gmail.com>
2017-10-10 11:37:33 +0800

    add

--- lovita/js/lovita.js
+++ lovita/js/lovita.js
@@ -1,5 +1,8 @@
 (function($) {
     $('.ring img').on('load', function() {
         $('.ring .rotate, .drop-animation').addClass('js-animate');
+        setTimeOut(function(){
+            $('.ring .rotate').removeClass('js-animate');
+        }, 1);
     });
 })(Zepto);

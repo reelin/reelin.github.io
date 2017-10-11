dda072c404407dde7522021e128e400f49de2c38
reelin <reelin.rl@gmail.com>
2017-10-11 11:09:54 +0800

    add

--- lovita/js/lovita.js
+++ lovita/js/lovita.js
@@ -10,35 +10,35 @@
     var isSlideDown = false;
     var minRange = 10;
     var isFirst = true;
-    // $('body').on('touchstart', function(e) {
-    //     clientY_start = e.touches[0].clientY;
-    // });
-    // $('body').on('touchend', function(e) {
-    //     e.preventDefault();
-    //     if (isSlideDown && isFirst) {
-    //         isFirst = false;
-    //         $('.inner-container .banner .ring').hide();
-    //         $('.drop-animation').addClass('js-animate');
-    //         $('.inner-container .banner .js-rotate').show().addClass('ring-animate');
-    //         setTimeout(function() {
-    //             $('.inner-container .banner .ring').show();
-    //             $('.inner-container .banner .js-rotate').hide().removeClass('ring-animate');
-    //         }, 5000);
-    //     }
-    //
-    // });
-    // $('body').on('touchmove',function (e) {
-    //     e.preventDefault();
-    //     clientY_end = e.changedTouches[0].clientY;
-    //     //判断移动的方向
-    //     // distance = clientY_end - clientY_start;
-    //     if(clientY_start + minRange < clientY_end) {
-    //        isSlideDown = false;
-    //     }
-    //     else if(clientY_start - minRange > clientY_end){
-    //        isSlideDown = true;
-    //     }
-    // });
+    $('.inner-container').on('touchstart', function(e) {
+        clientY_start = e.touches[0].clientY;
+    });
+    $('.inner-container').on('touchend', function(e) {
+        e.preventDefault();
+        if (isSlideDown && isFirst) {
+            isFirst = false;
+            $('.inner-container .banner .ring').hide();
+            $('.drop-animation').addClass('js-animate');
+            $('.inner-container .banner .js-rotate').show().addClass('ring-animate');
+            setTimeout(function() {
+                $('.inner-container .banner .ring').show();
+                $('.inner-container .banner .js-rotate').hide().removeClass('ring-animate');
+            }, 5000);
+        }
+
+    });
+    $('.inner-container').on('touchmove',function (e) {
+        e.preventDefault();
+        clientY_end = e.changedTouches[0].clientY;
+        //判断移动的方向
+        // distance = clientY_end - clientY_start;
+        if(clientY_start + minRange < clientY_end) {
+           isSlideDown = false;
+        }
+        else if(clientY_start - minRange > clientY_end){
+           isSlideDown = true;
+        }
+    });
     $('.thumb-block .text-block a').on('touchstart', function(e) {
         e.preventDefault();
 
